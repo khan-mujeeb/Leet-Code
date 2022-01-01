1d4f65c5146901e2f9f0d80a735e6df2226e4271
Mujeeb Khan <89351750+khan-mujeeb@users.noreply.github.com>
2022-01-01 15:53:07 +0530

    Add files via upload

---
+++ Reverse Integer/code.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+class Solution {
+public:
+    int reverse(int x) {
+       int ans=0;
+        while(x!=0){
+            int rem=x%10;
+            if(ans>INT_MAX/10||ans<INT_MIN/10)
+                return 0;
+            ans=(ans*10)+rem;
+            x/=10;
+        }
+        return ans;
+    }
+};
+
+/*Given a signed 32-bit integer x, return x with its digits reversed.
+If reversing x causes the value to go outside the signed 32-bit integer 
+range [-231, 231 - 1], then return 0.*/
+
+/*
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+*/
