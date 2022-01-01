09938d71ffbacd3f92dec87c5b09300721ce4462
Mujeeb Khan <89351750+khan-mujeeb@users.noreply.github.com>
2022-01-01 12:49:08 +0530

    Update and rename Reverse Integer to Reverse_Integer.cpp

--- Reverse_Integer.cpp
+++ Reverse_Integer.cpp
@@ -16,3 +16,18 @@ public:
 /*Given a signed 32-bit integer x, return x with its digits reversed.
 If reversing x causes the value to go outside the signed 32-bit integer 
 range [-231, 231 - 1], then return 0.*/
+
+/*
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+*/
