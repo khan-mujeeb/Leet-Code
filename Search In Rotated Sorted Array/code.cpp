3df77f504089f405e569adef252c88ea67c93480
Mujeeb Khan <89351750+khan-mujeeb@users.noreply.github.com>
2022-01-04 17:19:44 +0530

    Add files via upload

---
+++ Search In Rotated Sorted Array/code.cpp
@@ -0,0 +1,89 @@
+
+
+int getPivot(vector<int>& arr, int n) {
+
+    int s = 0;
+    int e = n-1;
+    int mid = s + (e-s)/2;
+
+    while(s<e) {
+
+        if(arr[mid] >= arr[0])
+        {
+            s = mid+1;
+        }
+        else{
+            e = mid;
+        }
+        mid = s + (e-s)/2;
+    }
+    return s;
+}
+
+int binarySearch(vector<int>& arr, int s, int e, int key) {
+
+    int start = s;
+    int end = e;
+
+    int mid = start + (end-start)/2;
+
+    while(start <= end) {
+
+        if(arr[mid] == key) {
+            return mid;
+        }
+
+  
+        if(key > arr[mid]) {
+            start = mid + 1;
+        }
+        else{ //key < arr[mid]
+            end = mid - 1;
+        }
+
+        mid = start + (end-start)/2;
+    }
+    
+    return -1;
+}
+
+
+
+int findPosition(vector<int>& arr, int n, int k)
+{
+    int pivot = getPivot(arr, n);
+    if( k >= arr[pivot] && k <= arr[n-1])
+    {//BS on second line
+        return binarySearch(arr, pivot, n-1, k);
+    }
+    else
+    {//BS on first line
+        return binarySearch(arr, 0, pivot - 1, k);
+    }
+    
+}
+
+
+/*Sample Input 1:
+2
+4 3
+8 9 4 5
+7 2
+2 4 5 6 8 9 1
+Sample Output 1:
+-1 
+0
+Explanation Of Sample Output 1:
+For the first test case, 3 is not present in the array. Hence the output will be -1.
+
+For the second test case, the occurrence of 2 is at index 0. Hence the output is 0.
+Sample Input 2:
+2
+4 3
+2 3 5 8
+1 2
+2
+Sample Output 2:
+1
+0
+*/
